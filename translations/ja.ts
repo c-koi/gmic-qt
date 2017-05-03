cfb09b47a9532c2adc286b9434bf4a3488d179d0
Sebastien Fourey <Sebastien.Fourey@ensicaen.fr>
2017-05-03 14:08:37 +0200

    Fix README.md

--- translations/ja.ts
+++ translations/ja.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 
-<!-- Original Japanese translation done by tokyogeometry (github) -->
+<!-- Original Japanese translation done by omiya tou (tokyogeometry / github) -->
 
 <!DOCTYPE TS>
 <TS version="2.1" language="ja">
