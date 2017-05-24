28155893a45a3a8d920d2d6933a978f222a8d0ba
Sebastien Fourey <Sebastien.Fourey@ensicaen.fr>
2017-05-24 18:46:08 +0200

    Change MainWindow title

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -89,9 +89,10 @@ MainWindow::MainWindow(QWidget *parent) :
 #define BETA_SUFFIX ""
 #endif
 
-  setWindowTitle(QString("G'MIC-Qt %1 - %2.%3.%4" BETA_SUFFIX )
+  setWindowTitle(QString("G'MIC-Qt %1- %5 %6 bits - %2.%3.%4" BETA_SUFFIX )
                  .arg(GmicQt::HostApplicationName.isEmpty() ? QString() : QString("for %1 ").arg(GmicQt::HostApplicationName))
-                 .arg(gmic_version/100).arg((gmic_version/10)%10).arg(gmic_version%10));
+                 .arg(gmic_version/100).arg((gmic_version/10)%10).arg(gmic_version%10)
+                 .arg(cimg_library::cimg::stros()).arg(sizeof(void*)==8?64:32));
 
   QStringList tsp = QIcon::themeSearchPaths();
   tsp.append(QString("/usr/share/icons/gnome"));
