7c740589a53f6f0058c4681d7651dd6b38236704
Sebastien Fourey <Sebastien.Fourey@ensicaen.fr>
2018-06-22 09:53:25 +0200

    Update check_version bash script

--- check_versions.sh
+++ check_versions.sh
@@ -2,6 +2,12 @@
 #
 # Usage: check_version.sh GMIC_PATH [gmic|CImg|check]
 #
+if [ $# != 2 ]; then
+    echo
+    echo "  Usage:"
+    echo "        check_version.sh GMIC_PATH [gmic|CImg|check]"
+    echo
+fi
 if [ "$2" = gmic ]; then
     grep "define gmic_version " $1/gmic.h | cut -d' ' -f3
 fi
@@ -11,9 +17,5 @@ fi
 if [ "$2" = check ]; then
     CIMG_VERSION=$(grep "define cimg_version " $1/CImg.h | cut -d' ' -f3)
     GMIC_VERSION=$(grep "define gmic_version " $1/gmic.h | cut -d' ' -f3)
-    if [ "${CIMG_VERSION}" = "${GMIC_VERSION}" ]; then
-	exit 0
-    else
-	exit 1
-    fi
+    test "${CIMG_VERSION}" = "${GMIC_VERSION}"
 fi
