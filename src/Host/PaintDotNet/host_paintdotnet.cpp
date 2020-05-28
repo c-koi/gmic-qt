323cfc5f66abe2834b106268e62e63ec61681c6c
Sebastien Fourey <Sebastien.Fourey@ensicaen.fr>
2020-05-28 19:28:47 +0200

    Customize available Input/Output modes for Paint.NET host

--- src/Host/PaintDotNet/host_paintdotnet.cpp
+++ src/Host/PaintDotNet/host_paintdotnet.cpp
@@ -158,6 +158,20 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //  disableInputMode(GmicQt::NoInput);
+    disableInputMode(GmicQt::Active);
+    //  disableInputMode(GmicQt::All);
+    disableInputMode(GmicQt::ActiveAndBelow);
+    disableInputMode(GmicQt::ActiveAndAbove);
+    disableInputMode(GmicQt::AllVisible);
+    disableInputMode(GmicQt::AllInvisible);
+
+    // disableOutputMode(GmicQt::InPlace);
+    disableOutputMode(GmicQt::NewImage);
+    disableOutputMode(GmicQt::NewLayers);
+    disableOutputMode(GmicQt::NewActiveLayers);
+
+
     QImage firstLayer(firstLayerImagePath);
 
     if (!firstLayer.isNull())
