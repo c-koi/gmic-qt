amyspark/gmic

Enable building with system libgmic
Signed-off-by: L. E. Segovia <amy@amyspark.me>

gmic-qt/src/FilterSelector/FiltersModelReader.cpp
@@ -38,6 +38,9 @@
 #include "LanguageSettings.h"
 #include "Logger.h"
 #include "Utils.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace GmicQt

gmic-qt/src/FilterThread.cpp
@@ -29,6 +29,9 @@
 #include "GmicStdlib.h"
 #include "Logger.h"
 #include "Misc.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace GmicQt

gmic-qt/src/GmicQt.cpp
@@ -45,6 +45,9 @@
 #include "Updater.h"
 #include "Widgets/InOutPanel.h"
 #include "Widgets/ProgressInfoWindow.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 #ifdef _IS_MACOS_
 #include <libgen.h>

gmic-qt/src/GmicStdlib.cpp
@@ -31,6 +31,9 @@
 #include <QStringList>
 #include "Common.h"
 #include "Utils.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace GmicQt

gmic-qt/src/HeadlessProcessor.cpp
@@ -41,6 +41,9 @@
 #include "ParametersCache.h"
 #include "Updater.h"
 #include "Widgets/ProgressInfoWindow.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 #ifdef _IS_WINDOWS_

gmic-qt/src/Host/None/ImageDialog.cpp
@@ -31,6 +31,9 @@
 #include <QStringList>
 #include "Common.h"
 #include "JpegQualityDialog.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace gmic_qt_standalone

gmic-qt/src/Host/None/host_none.cpp
@@ -40,6 +40,9 @@
 #include "GmicQt.h"
 #include "Host/GmicQtHost.h"
 #include "Host/None/ImageDialog.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 #define STRINGIFY(X) #X

gmic-qt/src/MainWindow.cpp
@@ -61,6 +61,9 @@
 #include "Utils.h"
 #include "Widgets/VisibleTagSelector.h"
 #include "ui_mainwindow.h"
+#ifndef gmic_build
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace

c-koi/gmic-qt

Co-authored-by: Antonio Rojas <arojas@archlinux.org>

src/FilterSelector/FiltersModelReader.cpp
@@ -38,6 +38,9 @@
 #include "LanguageSettings.h"
 #include "Logger.h"
 #include "Utils.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace GmicQt

src/FilterThread.cpp
@@ -31,6 +31,9 @@
 #include "Misc.h"
 #include "PersistentMemory.h"
 #include "Settings.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace GmicQt

src/GmicQt.cpp
@@ -45,6 +45,9 @@
 #include "Updater.h"
 #include "Widgets/InOutPanel.h"
 #include "Widgets/ProgressInfoWindow.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 #ifdef _IS_MACOS_
 #include <libgen.h>

src/GmicStdlib.cpp
@@ -31,6 +31,9 @@
 #include <QStringList>
 #include "Common.h"
 #include "Utils.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace GmicQt

src/HeadlessProcessor.cpp
@@ -40,6 +40,9 @@
 #include "Settings.h"
 #include "Updater.h"
 #include "Widgets/ProgressInfoWindow.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 #ifdef _IS_WINDOWS_

src/Host/None/ImageDialog.cpp
@@ -31,6 +31,9 @@
 #include <QStringList>
 #include "Common.h"
 #include "JpegQualityDialog.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace gmic_qt_standalone

src/Host/None/host_none.cpp
@@ -40,6 +40,9 @@
 #include "GmicQt.h"
 #include "Host/GmicQtHost.h"
 #include "Host/None/ImageDialog.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 #define STRINGIFY(X) #X

src/MainWindow.cpp
@@ -64,6 +64,9 @@
 #include "Utils.h"
 #include "Widgets/VisibleTagSelector.h"
 #include "ui_mainwindow.h"
+#ifndef gmic_core
+#include "CImg.h"
+#endif
 #include "gmic.h"
 
 namespace