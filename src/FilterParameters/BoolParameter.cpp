69ad94029fadcdf9d6f472d1074ecbd6911a1ffc
Sébastien Fourey <Sebastien.Fourey@ensicaen.fr>
2024-04-23 11:30:54 +0200

    Fix behavior of 'bool' parameters w.r.t. randomization

--- src/FilterParameters/BoolParameter.cpp
+++ src/FilterParameters/BoolParameter.cpp
@@ -106,7 +106,9 @@ void BoolParameter::randomize()
 {
   if (acceptRandom()) {
     _value = QRandomGenerator::global()->bounded(0, 2);
+    disconnectCheckBox();
     _checkBox->setChecked(_default);
+    connectCheckBox();
   }
 }
 
